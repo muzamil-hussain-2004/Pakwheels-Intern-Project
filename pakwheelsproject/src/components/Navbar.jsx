4ce1b996d5621a10ffd02ad038a9b53a2cbcffb7
muzamil hussain <muzamilhussain30072004@gmail.com>
2025-07-20 13:04:31 +0500

    update in navbar component

--- pakwheelsproject/src/components/Navbar.jsx
+++ pakwheelsproject/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(setQuery(searchInput)); 
-    navigate("/"); 
+    navigate("/cars"); 
   };
 
   return (
